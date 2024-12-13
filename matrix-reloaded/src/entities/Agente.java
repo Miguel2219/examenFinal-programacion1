4b4d853a457ba72f67030016ca3b86650970098a
Miguel Angel <lmangelryl1512@gmail.com>
2024-12-13 13:57:05 -0500

    Finalizar parcial

---
+++ matrix-reloaded/src/entities/Agente.java
@@ -0,0 +1,36 @@
+package entities;
+
+public class Agente extends Entidad {
+    private String habilidadEspecial;
+    private String misionAsignada;
+    private int nivel;
+
+    public Agente(int id, String nombre, String habilidadEspecial) {
+        super(id, nombre);
+        this.habilidadEspecial = habilidadEspecial;
+        this.nivel=1;
+    }
+    
+
+    public String getHabilidadEspecial() {
+        return habilidadEspecial;
+    }
+
+    public void setHabilidadEspecial(String habilidadEspecial) {
+        this.habilidadEspecial = habilidadEspecial;
+    }
+
+    public String getMisionAsignada() {
+        return misionAsignada;
+    }
+
+    public void asignarMision(String mision) {
+        this.misionAsignada = mision;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Habilidad Especial: " + habilidadEspecial +
+                (misionAsignada != null ? ", Misión: " + misionAsignada : "");
+    }
+}
